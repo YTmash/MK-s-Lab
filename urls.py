2656dd82600f859947f7a23205568a3ba6516a4f
YTmash <159938161+YTmash@users.noreply.github.com>
2025-02-11 21:26:48 -0600

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('sport_list/', views.sport_list, name='sport_list'),
-    path('contact/', views.contact, name='contact')
-]
